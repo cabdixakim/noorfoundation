da0895158a071a61bd0bae01ba600c457b2ae9c5
Abdihakim <cabdixakimsalan@gmail.com>
2020-08-07 23:59:26 +0300

    adding admin seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+use Carbon\Carbon;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        DB::table('users')->insert([
+            'username' => 'Admin',
+            'user_type' => 'admin',
+            'email' => 'admin@gmail.com',
+            'created_at' => Carbon::now(),
+            'email_verified_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'remember_token'=> null,
+            'password' => Hash::make('password'),
+        ]);
     }
 }
