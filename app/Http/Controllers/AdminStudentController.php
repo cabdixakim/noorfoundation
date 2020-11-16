d12066f69cc4e35e6e6f9c4beb84535f6d3251ce
Abdihakim <cabdixakimsalan@gmail.com>
2020-11-17 02:45:45 +0300

    fixed

--- app/Http/Controllers/AdminStudentController.php
+++ app/Http/Controllers/AdminStudentController.php
@@ -8,6 +8,11 @@ use Illuminate\Support\Facades\Redirect;
 
 class AdminStudentController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth','admin']);
+ 
+    }
     /**
      * Display a listing of the resource.
      *
--- app/Http/Controllers/SponsoredStudentsController.php
+++ app/Http/Controllers/SponsoredStudentsController.php
@@ -9,6 +9,11 @@ use Illuminate\Support\Facades\Auth;
 
 class SponsoredStudentsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth','admin','sponsor']);
+ 
+    }
     /**
      * Display a listing of the resource.
      *
