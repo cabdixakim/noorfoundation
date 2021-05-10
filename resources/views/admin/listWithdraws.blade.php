266940ee0fb230ccf2d0c73cdc832c09b6181c7b
Abdihakim <cabdixakimsalan@gmail.com>
2021-05-11 02:09:35 +0300

    removed deposits error

--- resources/views/admin/listWithdraws.blade.php
+++ resources/views/admin/listWithdraws.blade.php
@@ -29,31 +29,36 @@
           <div class="sm:w-2/5 mt-3 text-center">
           <span class="font-bold text-md"> {{\Carbon\Carbon::parse($key)->format('F , Y')}} </span> <span class="pl-2 italic">Total:</span> <span class="font-bold text-red-400 font-mono pl-2"> - ${{$withdrawals->sum('amount')}}</span> 
           </div>
-        <div class="container table-responsive pt-3 sm:w-3/5"> 
-            <table class="table table-bordered table-hover">
-              <thead class="thead-light">
-                <tr>
-                  <th scope="col">Date </th>
-                  <th scope="col">withdrawn by</th>
-                  <th scope="col">Amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($withdrawals as $withdrawal)
-                <tr>
-                <td>{{$withdrawal->created_at}}</td>
-                @if (!empty($withdrawal->student->profile))
-                 <td>{{ucwords($withdrawal->student->profile->firstname.' '.$withdrawal->student->profile->middlename.' '.$withdrawal->student->profile->lastname)}}</td>   
-                @else
-                <td>{{ucwords($withdrawal->student->username)}}</td>   
-                @endif
-                <td class="pl-5 text-red-400"> - ${{$withdrawal->amount}}</td>
-                </tr>
-                @endforeach
-              
-              </tbody>
-            </table>
+          @if ($withdrawals->isEmpty())
+          <h1 class="mt-5 text-center text-xl font-bold text-gray-400"> There are no withdrawals yet!</h1>
+          @else
+          <div class="container table-responsive pt-3 sm:w-3/5"> 
+              <table class="table table-bordered table-hover">
+                <thead class="thead-light">
+                  <tr>
+                    <th scope="col">Date </th>
+                    <th scope="col">withdrawn by</th>
+                    <th scope="col">Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($withdrawals as $withdrawal)
+                  <tr>
+                  <td>{{$withdrawal->created_at}}</td>
+                  @if (!empty($withdrawal->student->profile))
+                  <td>{{ucwords($withdrawal->student->profile->firstname.' '.$withdrawal->student->profile->middlename.' '.$withdrawal->student->profile->lastname)}}</td>   
+                  @else
+                  <td>{{ucwords($withdrawal->student->username)}}</td>   
+                  @endif
+                  <td class="pl-5 text-red-400"> - ${{$withdrawal->amount}}</td>
+                  </tr>
+                  @endforeach
+                
+                </tbody>
+              </table>
             </div>
+          @endif
+       
             @endforeach
      @endif
    
--- resources/views/randomUsersListDeposits.blade.php
+++ resources/views/randomUsersListDeposits.blade.php
@@ -26,31 +26,36 @@
       <div class="sm:w-2/5 mt-3 text-center">
       <span class="font-bold text-md"> {{\Carbon\Carbon::parse($key)->format('F , Y')}} </span> <span class="pl-2 italic">Total:</span> <span class="font-bold text-green-400 font-mono pl-2"> + ${{$deposits->sum('amount')}}</span> 
       </div>
-    <div class="container table-responsive pt-3 sm:w-3/5"> 
-        <table class="table table-bordered table-hover">
-          <thead class="thead-light">
-            <tr>
-              <th scope="col">Date </th>
-              <th scope="col">Deposited by</th>
-              <th scope="col">Amount</th>
-            </tr>
-          </thead>
-          <tbody>
-            @foreach ($deposits as $deposit)
-            <tr>
-            <td>{{$deposit->created_at}}</td>
-              @if (!empty($deposit->sponsor->profile))
-              <td>{{ucwords($deposit->sponsor->profile->firstname.' '.$deposit->sponsor->profile->middlename.' '.$deposit->sponsor->profile->lastname)}}</td>   
-              @else
-              <td>{{ucwords($deposit->sponsor->username)}}</td>   
-              @endif
-            <td class="pl-5 text-green-400"> + ${{$deposit->amount}}</td>
-            </tr>
-            @endforeach
-          
-          </tbody>
-        </table>
+      @if ($deposits->isEmpty())
+      <h1 class="mt-5 text-center text-xl font-bold text-gray-400"> There are no deposits yet!</h1>
+      @else
+      <div class="container table-responsive pt-3 sm:w-3/5"> 
+          <table class="table table-bordered table-hover">
+            <thead class="thead-light">
+              <tr>
+                <th scope="col">Date </th>
+                <th scope="col">Deposited by</th>
+                <th scope="col">Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($deposits as $deposit)
+              <tr>
+              <td>{{$deposit->created_at}}</td>
+                @if (!empty($deposit->sponsor->profile))
+                <td>{{ucwords($deposit->sponsor->profile->firstname.' '.$deposit->sponsor->profile->middlename.' '.$deposit->sponsor->profile->lastname)}}</td>   
+                @else
+                <td>{{ucwords($deposit->sponsor->username)}}</td>   
+                @endif
+              <td class="pl-5 text-green-400"> + ${{$deposit->amount}}</td>
+              </tr>
+              @endforeach
+            
+            </tbody>
+          </table>
         </div>
+      @endif
+    
         @endforeach
     @endif
    
